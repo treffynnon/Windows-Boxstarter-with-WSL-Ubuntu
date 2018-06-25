a74d7a6db9fba153957afb128856d681a3becee2
Simon Holywell <treffynnon@php.net>
2018-06-25 10:52:46 +1000

    Alias for rebooting WSL without rebooting Windows

--- config_files/homedir/.bashrc.d/system.sh
+++ config_files/homedir/.bashrc.d/system.sh
@@ -3,3 +3,6 @@ eval `dircolors ~/.dircolors/nord-dircolors-develop/src/dir_colors`
 
 # an OK command prompt for bash
 PS1='\[\033[0;32m\]\[\033[0m\033[0;32m\]\u\[\033[1;34m\] @ \[\033[1;34m\]\h \w\[\033[0;32m\]$(__git_ps1)\n\[\033[0;32m\]└─\[\033[0m\033[0;32m\]\[\033[0m\033[0;32m\] λ\[\033[0m\] '
+
+# restarting Ubuntu inside WSL
+alias reboot="sudo killall -r '.*'"
